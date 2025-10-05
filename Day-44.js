948b25973ca2245bc69d1ea0ea1d702469b6010b
shiva <kotteshivashankar9346@gmail.com>
2025-10-05 21:16:07 +0530

    day 44

---
+++ Day-44.js
@@ -0,0 +1,14 @@
+class Node{
+    constructor(data){
+        this.data=data;
+        this.next=null;
+        this.prev=null
+    }
+}
+class circularLinkedList{
+    constructor(){
+        this.head=null;
+        this.tail=null
+        
+    }
+}
