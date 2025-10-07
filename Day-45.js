e9bed0829589c9dd867c3df97a453e9e7cd53f0d
shiva <kotteshivashankar9346@gmail.com>
2025-10-07 11:03:00 +0530

    day 45 added

--- Day-45.js
+++ Day-45.js
@@ -11,4 +11,56 @@ class circularLinkedList{
         this.tail=null
         
     }
+
+    insertAtEnd(data){
+        const newNode=new Node(data);
+        if(!this.head){
+            this.head=this.tail=newNode;
+            newNode.next=newNode;
+        }else{
+            this.tail.next=newNode;
+            newNode.next=this.head;
+            this.tail=newNode
+        }
+    }
+
+    display(){
+        if(!this.head){
+            console.log("Empty");
+            return   
+        }
+        let result=[];
+        let current=this.head;
+        do{
+            result.push(current.data)
+            current=current.next;
+
+        }while(current !==this.head)
+            console.log(result.join("->"))
+    }
+
+    showPrinters(k){
+
+        if(this.head == null){
+            console.log("NO printers");
+            return;
+        }
+
+        let current=this.head;
+
+        for(let i=0;i<k;i++){
+             console.log(`Job Assgined to: ${current.data}`);
+             current = current.next; 
+
+        }
+
+    }
+
 }
+
+let list = new circularLinkedList();
+list.insertAtEnd("printer-1")
+list.insertAtEnd("printer-2")
+list.insertAtEnd("printer-3")
+list.display()
+list.showPrinters(5)
