e12b550eaa63fda9d82ac8df26b36b00debd9612
shiva <kotteshivashankar9346@gmail.com>
2025-10-14 18:36:26 +0530

    day 56 added

---
+++ Day-55.js
@@ -0,0 +1,41 @@
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+
+    push(element) {
+        this.items.push(element);
+    }
+
+    pop() {
+        if (this.isEmpty()) return null;
+        return this.items.pop();
+    }
+
+    peek() {
+        if (this.isEmpty()) return null;
+        return this.items[this.items.length - 1];
+    }
+
+    isEmpty() {
+        return this.items.length === 0;
+    }
+}
+
+
+function findGreater(arr){
+    let newStack=new Stack();
+
+    let result= new Array(arr.length).fill(-1);
+
+    for(let i=0;i<arr.length;i++){
+        while(!newStack.isEmpty() && arr[i] > arr[newStack.peek()] ){
+            let topIndex=newStack.pop();
+            result[topIndex]=arr[i]
+        }
+        newStack.push(i);  
+    }
+      return result;
+}
+console.log(findGreater([4, 5, 2, 25]));
+console.log(findGreater([13, 7, 6, 12]));
