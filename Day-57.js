990b01ea661263c003383b3032d4ef24dfe7efab
shiva <kotteshivashankar9346@gmail.com>
2025-10-15 22:44:30 +0530

    day 57 added

--- Day-57.js
+++ Day-57.js
@@ -21,9 +21,37 @@ class Stack {
         return this.items.length === 0;
     }
 }
-function encodeString(n) {
 
-    let stack = new Stack();
-    
-    
+function decodeString(s) {
+    let numStack = new Stack();  
+    let strStack = new Stack();  
+    let currStr = "";
+    let currNum = 0;
+
+    for (let ch of s) {
+        if (!isNaN(ch)) {
+            currNum = currNum * 10 + Number(ch);
+        } 
+        else if (ch === "[") {
+            numStack.push(currNum);
+            strStack.push(currStr);
+            currNum = 0;
+            currStr = "";
+        } 
+        else if (ch === "]") {
+            let repeatTimes = numStack.pop();
+            let prevStr = strStack.pop();
+            currStr = prevStr + currStr.repeat(repeatTimes);
+        } 
+        else {
+            currStr += ch;
+        }
+    }
+
+    return currStr;
 }
+
+
+console.log(decodeString("3[a]2[bc]"));     
+console.log(decodeString("3[a2[c]]"));      
+console.log(decodeString("2[abc]3[cd]ef"));
